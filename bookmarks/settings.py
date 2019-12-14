4dd5497879cc484e72bccdd438426a2ff4eaf053
hurte <hurte@foxmail.com>
2019-12-14 23:19:06 +0800

    comm

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -143,3 +143,4 @@ LOGOUT_URL = 'logout'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_CHARSET = "UTF-8"
